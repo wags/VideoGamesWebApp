0414a93c19a415264b9a9e0d7e95a7b8e6d3e828
Matt Wagner <matt@wags.us>
2017-08-15 16:12:31 -0400

    Move video game data to a private static variable

--- ApiControllers/VideoGamesController.cs
+++ ApiControllers/VideoGamesController.cs
@@ -11,28 +11,28 @@ namespace WebApp.ApiControllers
     [Route("api/[controller]")]
     public class VideoGamesController : Controller
     {
+        private static List<VideoGame> _videoGames = new List<VideoGame>()
+        {
+            new VideoGame()
+            {
+                Id = 1,
+                Title = "Super Mario 64",
+                PublishedOn = new DateTime(1996, 1, 1),
+                Platform = "N64"
+            },
+            new VideoGame()
+            {
+                Id = 2,
+                Title = "Resident Evil",
+                PublishedOn = new DateTime(1994, 1, 1),
+                Platform = "Playstation"
+            }
+        };
+
         [HttpGet]
         public IActionResult Get()
         {
-            var videoGames = new List<VideoGame>()
-            {
-                new VideoGame()
-                {
-                   Id = 1,
-                   Title = "Super Mario 64",
-                   PublishedOn = new DateTime(1996, 1, 1),
-                   Platform = "N64"
-                },
-                new VideoGame()
-                {
-                    Id = 2,
-                    Title = "Resident Evil",
-                    PublishedOn = new DateTime(1994, 1, 1),
-                    Platform = "Playstation"
-                }
-            };
-
-            return Ok(videoGames);
+            return Ok(_videoGames);
         }
     }
 }
