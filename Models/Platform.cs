9ca6f92ab87185856f35fe7bf81f12c0d7f31f18
Matt Wagner <matt@wags.us>
2017-08-21 10:40:55 -0400

    Update platform model to contain list of video games

--- Models/Platform.cs
+++ Models/Platform.cs
@@ -1,8 +1,16 @@
+using System.Collections.Generic;
+
 namespace WebApp.Models
 {
     public class Platform
     {
+        public Platform()
+        {
+            VideoGames = new List<VideoGame>();
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
+        public IList<VideoGame> VideoGames { get; set; }
     }
 }
